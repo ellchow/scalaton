d17b3869114556757c6feb243970333e9701158a
Elliot Chow <drag0nxspirit@gmail.com>
2013-01-20 12:39:03 -0800

    reorganize bloom filter code

--- src/main/scala/stats/singlepass/BloomFilter.scala
+++ src/main/scala/stats/singlepass/BloomFilter.scala
@@ -4,7 +4,7 @@ import scala.language.postfixOps
 
 import scala.collection.BitSet
 
-import scalaz._
+import scalaz.{BloomFilter => _, _}
 import Scalaz._
 
 import scalaton.util._
@@ -12,19 +12,34 @@ import scalaton.util.hashable._
 
 object bloomfilter{
 
-  val emptyBitSet = BitSet.empty
-  /*
-  def bfmInstance[A,B](numItems: Int, fpProb: Double, s: Long = 0L)
-                      (implicit h: Hashable[A, B],
-                       hconv: HashCodeConverter[B, Int]): Monoid[BloomFilter[A,B]] with Equal[BloomFilter[A,B]] = {
-    val (numHashes, width) = BloomFilter.optimalParameters(numItems, fpProb)
-    bfmInstance(numHashes, width, s)(h, hconv)
-  }
-  */
+  def BloomFilter[A, B](numItems: Int, fpProb: Double,
+                  seed: Long = 0L)(items: A*)
+                                  (implicit h: Hashable[A, B],
+                                   hconv: HashCodeConverter[B, Int]): BloomFilter[A,B] = {
+    val (numHashes, width) = optimalParameters(numItems, fpProb)
 
-  def bfmInstance[A,B](numHashes: Int, width: Int, s: Long = 0L)
-                      (implicit h: Hashable[A, B],
-                       hconv: HashCodeConverter[B, Int]): Monoid[BloomFilter[A,B]] with Equal[BloomFilter[A,B]] =
+    val z: BloomFilter[A,B] = BFZero(numHashes, width, seed)(h, hconv)
+
+    items.foldLeft(z)((acc, x) => acc + x)
+  }
+
+  def optimalParameters(numItems: Int, fpProb: Double) = {
+    val width = optimalWidth(numItems, fpProb)
+    val numHashes = optimalNumHashes(numItems, width)
+
+    (numItems, width)
+  }
+
+  def optimalNumHashes(numItems: Int, width: Int): Int =
+    math.ceil(width / numItems * math.log(2)).toInt + 1
+
+  def optimalWidth(numItems: Int, fpProb: Double): Int =
+    math.ceil(-1 * numItems * math.log(fpProb) / math.log(2) / math.log(2)).toInt + 1
+
+
+  def BloomFilterMonoid[A,B](numHashes: Int, width: Int, s: Long = 0L)
+                            (implicit h: Hashable[A, B],
+                             hconv: HashCodeConverter[B, Int]): Monoid[BloomFilter[A,B]] with Equal[BloomFilter[A,B]] =
     new Monoid[BloomFilter[A,B]] with Equal[BloomFilter[A,B]]{
 
       def equal(bf1: BloomFilter[A,B], bf2: BloomFilter[A,B]) = {
@@ -47,113 +62,93 @@ object bloomfilter{
         bf1 ++ bf2
     }
 
-  sealed trait BloomFilter[A,B]{
-    val numHashes: Int
-    val width: Int
-    val s: Long
-
-    def hasSameParameters(other: BloomFilter[A,B]) =
-      (numHashes === other.numHashes) && (width === other.width) && (s === other.s)
-
-    def + (item: A): BloomFilter[A, B]
-
-    def ++ (other: BloomFilter[A, B]): BloomFilter[A,B]
-
-    def contains(item: A): Boolean
-  }
-
-  case class BFZero[A,B](val numHashes: Int,
-                         val width: Int,
-                         val s: Long = 0L) // cannot put Long @@ HashSeed for some reason...)
-                        (implicit h: Hashable[A, B],
-                         hconv: HashCodeConverter[B, Int])
-
-  extends BloomFilter[A, B]{
-    val bits = emptyBitSet
-
-    def + (item: A): BloomFilter[A, B] =
-      BFInstance(numHashes,width,s=s)(h,hconv) + item
-
-
-    def ++ (other: BloomFilter[A, B]) = {
-      require(hasSameParameters(other),
-              "cannot combine bloom filters with different parameters")
-      other
-    }
-
-    def contains(item: A): Boolean = false
-  }
-
-  case class BFInstance[A, B](val numHashes: Int,
-                              val width: Int,
-                              val bits: BitSet = emptyBitSet,
-                              val s: Long = 0L // cannot put Long @@ HashSeed for some reason...
-                            )
-                             (implicit h: Hashable[A, B],
-                              hconv: HashCodeConverter[B, Int])
-       extends BloomFilter[A, B]
-  {
-
-    def seed: Long @@ HashSeed = HashSeed(s)
-
-    def hashItem(item: A): Seq[Int @@ HashCode] = {
-      val hcs = (multiHash(item, seed)(h) |> hconv.convertSeq) take numHashes
-      hcs.toSeq map { _ % width |> HashCode }
-    }
-
-    def + (item: A): BloomFilter[A, B] = {
-      val hcs = hashItem(item)
-      val itemBits: BitSet = BitSet(hcs : _*)
-      construct(bits ++ itemBits)
-    }
-
-    def ++ (other: BloomFilter[A, B]) = other match {
-      case z : BFZero[A,B] => this
-      case bf: BFInstance[A,B] => {
-        require(hasSameParameters(other))
-        construct(bits ++ bf.bits)
-      }
-    }
-
-    def contains(item: A): Boolean = {
-      val hcs = hashItem(item)
-      val itemBits: BitSet = BitSet(hcs : _*)
-
-      (bits & itemBits) == itemBits
-    }
-
-    private def construct(b: BitSet): BloomFilter[A, B] =
-      BFInstance(numHashes, width, b, s)(h, hconv)
-  }
-
-  object BloomFilter{
-
-    def apply[A, B](numItems: Int, fpProb: Double,
-                    seed: Long = 0L)(items: A*)
-               (implicit h: Hashable[A, B],
-                hconv: HashCodeConverter[B, Int]): BloomFilter[A,B] = {
-      val (numHashes, width) = optimalParameters(numItems, fpProb)
-
-      val z: BloomFilter[A,B] = BFZero(numHashes, width, seed)(h, hconv)
-
-      items.foldLeft(z)((acc, x) => acc + x)
-    }
-
-    def optimalParameters(numItems: Int, fpProb: Double) = {
-      val width = optimalWidth(numItems, fpProb)
-      val numHashes = optimalNumHashes(numItems, width)
-
-      (numItems, width)
-    }
-
-
-    def optimalNumHashes(numItems: Int, width: Int): Int =
-      math.ceil(width / numItems * math.log(2)).toInt + 1
-
-    def optimalWidth(numItems: Int, fpProb: Double): Int =
-      math.ceil(-1 * numItems * math.log(fpProb) / math.log(2) / math.log(2)).toInt + 1
-
-  }
-
 }
+
+sealed trait BloomFilter[A,B]{
+  val numHashes: Int
+  val width: Int
+  val s: Long
+
+  def hasSameParameters(other: BloomFilter[A,B]) =
+  (numHashes === other.numHashes) && (width === other.width) && (s === other.s)
+
+  def hashItem(item: A)(implicit h: Hashable[A, B],
+                        hconv: HashCodeConverter[B, Int]): BitSet = {
+    val hcs = (multiHash(item, HashSeed(s))(h) |> hconv.convertSeq) take numHashes
+    BitSet(hcs.toSeq map { _ % width |> HashCode } : _*)
+  }
+
+  def + (item: A): BloomFilter[A, B]
+
+  def ++ (other: BloomFilter[A, B]): BloomFilter[A,B]
+
+  def contains(item: A): Boolean
+}
+
+case class BFZero[A,B](val numHashes: Int,
+                       val width: Int,
+                       val s: Long = 0L) // cannot put Long @@ HashSeed for some reason...)
+                      (implicit h: Hashable[A, B],
+                       hconv: HashCodeConverter[B, Int])
+extends BloomFilter[A, B]{
+
+  def + (item: A): BloomFilter[A, B] =
+    BFInstance(numHashes,width,hashItem(item),s)(h,hconv)
+
+
+  def ++ (other: BloomFilter[A, B]) = {
+    require(hasSameParameters(other),
+            "cannot combine bloom filters with different parameters")
+    other
+  }
+
+  def contains(item: A): Boolean = false
+}
+
+case class BFInstance[A, B](val numHashes: Int,
+                            val width: Int,
+                            val bits: BitSet,
+                            val s: Long = 0L // cannot put Long @@ HashSeed for some reason...
+                          )
+                           (implicit h: Hashable[A, B],
+                            hconv: HashCodeConverter[B, Int])
+     extends BloomFilter[A, B]
+{
+
+  def + (item: A): BloomFilter[A, B] =
+    construct(bits ++ hashItem(item))
+
+
+  def ++ (other: BloomFilter[A, B]) = other match {
+    case z : BFZero[A,B] => this
+    case bf: BFInstance[A,B] => {
+      require(hasSameParameters(other))
+      construct(bits ++ bf.bits)
+    }
+  }
+
+  def contains(item: A): Boolean = {
+    val itemBits = hashItem(item)
+    (bits & itemBits) == itemBits
+  }
+
+  private def construct(b: BitSet): BloomFilter[A, B] =
+    BFInstance(numHashes, width, b, s)(h, hconv)
+}
+
+
+// trait BloomFilterMonoidInstances extends BloomFilterImpl{
+//   /*
+//    def bfmInstance[A,B](numItems: Int, fpProb: Double, s: Long = 0L)
+//    (implicit h: Hashable[A, B],
+//    hconv: HashCodeConverter[B, Int]): Monoid[BloomFilter[A,B]] with Equal[BloomFilter[A,B]] = {
+//    val (numHashes, width) = BloomFilter.optimalParameters(numItems, fpProb)
+//    bfmInstance(numHashes, width, s)(h, hconv)
+//    }
+//    */
+
+
+// }
+
+
 
