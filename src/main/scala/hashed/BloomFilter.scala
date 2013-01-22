902ae39fe0ad1cae46b83bcfd4bfa161dd755949
Elliot Chow <drag0nxspirit@gmail.com>
2013-01-21 23:36:53 -0800

    bloom filter - fix bugs in parameter setting; updated test cases

--- src/main/scala/hashed/BloomFilter.scala
+++ src/main/scala/hashed/BloomFilter.scala
@@ -11,6 +11,12 @@ import Scalaz._
 import scalaton.util._
 import scalaton.util.hashable._
 
+
+/**
+ * Bloom filter
+ *
+ * http://en.wikipedia.org/wiki/Bloom_filter
+ */
 trait BloomFilter[A,B,G[_],F <: G[_]]
 extends MakesSingleton[A,B,Int,F]
 with Contains[A,B,Int,F]{
@@ -20,8 +26,14 @@ with Contains[A,B,Int,F]{
   override def hashItem(item: A)(implicit h: Hashable[A, B],
                                  hconv: HashCodeConverter[B, Int]): Iterable[Int @@ HashCode] =
     super.hashItem(item)(h,hconv) map { _ % width |> HashCode}
+
 }
 
+
+
+/**
+ * Standard Bloom Filter
+ **/
 sealed trait SBF
 
 trait StandardBloomFilter[A,B] extends BloomFilter[A,B,SortedSet,BitSet @@ SBF] with Monoid[BitSet @@ SBF] with Equal[BitSet @@ SBF] {
@@ -55,10 +67,9 @@ with SizesFunctions{
 
     val empty: BitSet @@ SBF = Tag[BitSet, SBF](BitSet.empty)
 
-    def apply[A,B](k: Int, w: Int, s: Long) = new StandardBloomFilter[A,B]{
-      val numHashes: Int = k
+    def apply[A,B](params: (Int, Int), s: Long = 0L) = new StandardBloomFilter[A,B]{
 
-      val width: Int = w
+      val (numHashes, width) = params
 
       val seed: Long = s
 
@@ -70,6 +81,22 @@ with SizesFunctions{
       def append(sbf1: BitSet @@ SBF, sbf2: => BitSet @@ SBF): BitSet @@ SBF =
         Tag[BitSet, SBF](sbf1 ++ sbf2)
     }
+
+    /** http://en.wikipedia.org/wiki/Bloom_filter#Probability_of_false_positives **/
+    def optimalNumHashes(numItems: Int, width: Int): Int =
+      math.ceil(width / numItems * math.log(2)).toInt
+
+    /** http://en.wikipedia.org/wiki/Bloom_filter#Probability_of_false_positives **/
+    def optimalWidth(numItems: Int, fpProb: Double): Int =
+      math.ceil(-1 * numItems * math.log(fpProb) / math.log(2) / math.log(2)).toInt
+
+    def optimalParameters(numItems: Int, fpProb: Double) = {
+      val width = optimalWidth(numItems, fpProb)
+      val numHashes = optimalNumHashes(numItems, width)
+
+      (numHashes, width)
+    }
+
   }
 }
 
--- src/test/scala/StandardBloomFilterSpec.scala
+++ src/test/scala/StandardBloomFilterSpec.scala
@@ -1,4 +1,6 @@
-package scalaton
+package scalaton.hashed
+
+import scala.collection.BitSet
 
 import scala.util.{Random => SRandom}
 
@@ -8,133 +10,84 @@ import scalaz.{BloomFilter => _, _}
 import Scalaz._
 
 import scalaton.util.hashable._
-import scalaton.StandardBloomFilter
+import scalaton.hashed.bloomfilter._
 
 class StandardBloomFilterSpec extends Specification{
 
   "an empty bloom filter" should {
 
-    implicit val bfmon = StandardBloomFilter.monoid[String,(Long,Long)]((5,625))
-    val bfz: StandardBloomFilter[String,(Long,Long)] = BFZero[String,(Long,Long)](5, 625)
+    implicit val BF = StandardBloomFilter[String,(Long,Long)]((5,625), 0L)
 
     "not contain anything" in {
       SRandom.setSeed(0)
 
       0 to 1000 foreach { i =>
-        bfz contains (SRandom nextDouble() toString) must beFalse
+         contains(StandardBloomFilter.empty,
+                  SRandom nextDouble() toString) must beFalse
+      }
+    }
+
+    "is only equal to another empty bloom filter" in {
+      val bfz = Tag[BitSet,SBF](BitSet.empty)
+      (StandardBloomFilter.empty === bfz) must beTrue
+
+      0 to 1000 foreach { i =>
+        (StandardBloomFilter.empty === singleton(SRandom nextDouble() toString)) must beFalse
       }
     }
 
     "be empty when added with another empty bloom filter" in {
-      ((bfz |+| bfz) === bfz) must beTrue
+      ((StandardBloomFilter.empty |+| StandardBloomFilter.empty) === StandardBloomFilter.empty) must beTrue
     }
 
     "contain the item after adding it" in {
-      (bfz + "a" contains "a") must beTrue
+      contains(singleton("a"), "a") must beTrue
     }
 
     "be equal to the other bloom filter after add another" in {
-      val bf = bfz + "a"
-
-      ((bfz |+| bf) === bf) must beTrue
-    }
-
-    "is only equal to empty bloom filters with same parameters and without any bits set" in {
-      implicit val bfmon = StandardBloomFilter.monoid[String,(Long,Long)]((1,2),3)
-
-      val bfz1: StandardBloomFilter[String,(Long,Long)] =
-        BFZero(1, 2, 3)
-
-      val bfz2: StandardBloomFilter[String,(Long,Long)] =
-        BFZero(100, 2, 3)
-
-      val bfz3: StandardBloomFilter[String,(Long,Long)] =
-        BFZero(1, 100, 3)
-
-      val bfz4: StandardBloomFilter[String,(Long,Long)] =
-        BFZero(1, 2, 100)
-
-      val bf1: StandardBloomFilter[String,(Long,Long)] =
-        BFInstance[String,(Long,Long)](1,2,collection.BitSet.empty,3)
-
-      val bf2: StandardBloomFilter[String,(Long,Long)] =
-        BFInstance[String,(Long,Long)](1,2,collection.BitSet(1),3)
-
-      (bfz1 === bfz1) must beTrue
-      (bfz1 === bf1) must beTrue
-
-      (bfz1 === bf2) must beFalse
-
-      (bfz1 === bfz2) must throwA[IllegalArgumentException]
-      (bfz1 === bfz3) must throwA[IllegalArgumentException]
-      (bfz1 === bfz4) must throwA[IllegalArgumentException]
+      ((StandardBloomFilter.empty |+| singleton("a")) === singleton("a")) must beTrue
     }
 
   }
 
   "a nonempty bloom filter" should {
 
-    "is only equal to another nonempty bloom filter with same parameters and same bits set" in {
-      implicit val bfmon = StandardBloomFilter.monoid[String,(Long,Long)]((1,2),3)
-
-      val bf1: StandardBloomFilter[String,(Long,Long)] =
-        BFInstance[String,(Long,Long)](1,2,collection.BitSet(1),3)
-
-      val bf2: StandardBloomFilter[String,(Long,Long)] =
-          BFInstance[String,(Long,Long)](100,2,collection.BitSet(1),3)
-
-      val bf3: StandardBloomFilter[String,(Long,Long)] =
-          BFInstance[String,(Long,Long)](1,100,collection.BitSet(1),3)
-
-      val bf4: StandardBloomFilter[String,(Long,Long)] =
-          BFInstance[String,(Long,Long)](1,2,collection.BitSet(1),100)
-
-      val bf5: StandardBloomFilter[String,(Long,Long)] =
-          BFInstance[String,(Long,Long)](1,2,collection.BitSet(1,2),3)
-
-      (bf1 === bf1) must beTrue
-      (bf1 === bf5) must beFalse
-
-      (bf1 === bf2) must throwA[IllegalArgumentException]
-      (bf1 === bf3) must throwA[IllegalArgumentException]
-      (bf1 === bf4) must throwA[IllegalArgumentException]
-    }
-
     "should contain all true positives" in {
       SRandom.setSeed(0)
 
-      val BF = StandardBloomFilter[String, (Long,Long)](100, 0.05) _
+      val params = StandardBloomFilter.optimalParameters(100, 0.05)
+      implicit val BF = StandardBloomFilter[String, (Long,Long)](params, 0L)
+
       0 to 10 foreach { i =>
         val items = 0 to 10 map { _ => SRandom nextDouble() toString }
-        val bf = BF(items)
+        val bf = items.foldLeft(StandardBloomFilter.empty)((acc,x) => insert(acc,x))
 
-        items foreach { i => bf contains i must beTrue }
+        items foreach { i => contains(bf, i) must beTrue }
       }
     }
 
     "should be below false-positive rate with high confidence" in {
       SRandom.setSeed(0)
 
-      Seq((0.1, 0.15), (0.05, 0.075),
-          (0.01,0.015), (0.005, 0.01)
-        ) foreach{ case (fpProb, maxFp) =>
-        val fps = 0 until 15000 map { _ =>
-          val numItems = 20
-          // val items = 0 until numItems map { _ => SRandom nextDouble() toString }
-          // val test = SRandom nextDouble() toString
-          // val bf = StandardBloomFilter[String, (Long,Long)](numItems, fpProb)(items : _*)
+      Seq(0.1, 0.05, 0.01, 0.005) foreach{ fpProb =>
+        val numItems = 20
+        val params = StandardBloomFilter.optimalParameters(numItems, fpProb)
 
+        implicit val BF = StandardBloomFilter[(String, String), (Long,Long)](params, 0L)
+
+        val fps = 0 until 15000 map { _ =>
           val items = 0 until numItems map { _ => (SRandom nextDouble() toString,
                                                    SRandom nextDouble() toString) }
           val test = (SRandom nextDouble() toString, SRandom nextDouble() toString)
-          val bf = StandardBloomFilter[(String, String), (Long,Long)](numItems, fpProb)(items : _*)
 
-          if(bf contains test) 1.0 else 0.0
+          val bf = items.foldLeft(StandardBloomFilter.empty)((acc,x) => insert(acc,x))
+          if(contains(bf, test)) 1.0 else 0.0
         }
         val observed = fps.sum / fps.size
 
-        observed must beLessThan(maxFp)
+        observed must beLessThan(1.25 * fpProb)
       }
     }
+
   }
 }
