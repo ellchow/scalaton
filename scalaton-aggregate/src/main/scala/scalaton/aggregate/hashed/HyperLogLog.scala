1d1a81b58d6cd3680f0c7b76aefeb50f27488596
ellchow <drag0nxspirit@gmail.com>
2013-03-05 14:22:59 -0800

    HyperLogLog backed by sparse vector

--- scalaton-aggregate/src/main/scala/scalaton/aggregate/hashed/HyperLogLog.scala
+++ scalaton-aggregate/src/main/scala/scalaton/aggregate/hashed/HyperLogLog.scala
@@ -61,6 +61,8 @@ with Monoid[D @@ T]{
 
     val estimate = alphamm / rsum
 
+    println(numZeroRegisters(d) + "   " + m + "   " + "    " + estimate + "    " + (numZeroRegisters(d) =/= 0) + "    " + rsum)
+
     val correctedEstimate = if(estimate lte (5.0 / 2.0 * m)){ // small range correction
       val v = numZeroRegisters(d)
 
@@ -72,8 +74,6 @@ with Monoid[D @@ T]{
 
     math.round(correctedEstimate)
   }
-
-
 
 }
 
@@ -88,8 +88,10 @@ extends HyperLogLogT[A,H1,Vector[Int],T]{
   def append (d1: Vector[Int] @@ T, d2: => Vector[Int] @@ T): Vector[Int] @@ T =
     tag(d1.zip(d2) map { case (r1, r2) => r1 max r2 })
 
-  protected def registerSum(d: Vector[Int] @@ T) =
-    d map ( count => math.pow(2, (-1 * count))) sum
+  protected def registerSum(d: Vector[Int] @@ T) = {
+    d map ( count => math.pow(2, -count)) sum
+  }
+
 
   protected def numZeroRegisters(d: Vector[Int] @@ T) =
     d.foldLeft(0)((acc, x) => (x === 0) ? (acc + 1) | acc)
@@ -100,11 +102,46 @@ extends HyperLogLogT[A,H1,Vector[Int],T]{
 
 }
 
+trait SparseHyperLogLogT[A,H1,T]
+extends HyperLogLogT[A,H1,Map[Int,Int @@ Tags.Max],T]{
+
+  implicit val maxIntMonoid: Monoid[Int @@ Tags.Max] = Monoid instance ((l, r) => Tag(l max r), Tags.Max(Int.MinValue))
+
+  def equal(d1: Map[Int,Int @@ Tags.Max] @@ T, d2: Map[Int,Int @@ Tags.Max] @@ T) =
+    d1 == d2
+
+  lazy val zero = tag(Map.empty)
+
+  def append (d1: Map[Int,Int @@ Tags.Max] @@ T, d2: => Map[Int,Int @@ Tags.Max] @@ T): Map[Int,Int @@ Tags.Max] @@ T =
+    tag((d1: Map[Int,Int @@ Tags.Max]) |+| (d2: Map[Int,Int @@ Tags.Max]))
+
+  protected def registerSum(d: Map[Int,Int @@ Tags.Max] @@ T) = {
+    val nonZeroCountSum = d.map{ case (j, count) => math.pow(2, -count) } sum
+
+    numZeroRegisters(d) + nonZeroCountSum
+  }
+
+  protected def numZeroRegisters(d: Map[Int,Int @@ Tags.Max] @@ T) =
+    m - d.size
+
+  protected def registerValue(d: Map[Int,Int @@ Tags.Max] @@ T, j: Int) = d.get(j) | Tags.Max(0)
+
+  protected def updateRegister(d: Map[Int,Int @@ Tags.Max] @@ T, j: Int, n: Int) = tag(d.updated(j, Tags.Max(n)))
+}
+
+
+
 object hyperloglog{
   type HLLRegisters[T] = Vector[Int] @@ T
 
   object hll{
     def dense[A,H1,T](addressSize: Int, s: Long = 0L) = new DenseHyperLogLogT[A,H1,T]{
+      val seed = s
+
+      val b = addressSize
+    }
+
+    def sparse[A,H1,T](addressSize: Int, s: Long = 0L) = new SparseHyperLogLogT[A,H1,T]{
       val seed = s
 
       val b = addressSize
