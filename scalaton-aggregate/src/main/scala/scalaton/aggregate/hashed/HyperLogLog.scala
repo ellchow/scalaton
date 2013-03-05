1589fb9db90eb35b6416e7db8e667afd6a4f49f0
ellchow <drag0nxspirit@gmail.com>
2013-03-05 11:57:12 -0800

    make class for dense hll impl

--- scalaton-aggregate/src/main/scala/scalaton/aggregate/hashed/HyperLogLog.scala
+++ scalaton-aggregate/src/main/scala/scalaton/aggregate/hashed/HyperLogLog.scala
@@ -7,27 +7,26 @@ import scalaton.util._
 import scalaton.util.hashing._
 
 /** Hyper log log implementation using 32 bit hash (http://algo.inria.fr/flajolet/Publications/FlFuGaMe07.pdf).  Good for cardinalities from 0 to 10^9 **/
-trait HyperLogLogT[A,H1,T]
+trait HyperLogLogT[A,H1,D,T]
 extends HashedCollection[A,H1,Bits32]
-with InsertsElement[A,H1,Bits32,Vector[Int] @@ T]
-with Sized[A,H1,Bits32,Vector[Int] @@ T]
-with Equal[Vector[Int] @@ T]
-with Monoid[Vector[Int] @@ T]{
-
+with InsertsElement[A,H1,Bits32,D @@ T]
+with Sized[A,H1,Bits32,D @@ T]
+with Equal[D @@ T]
+with Monoid[D @@ T]{
   val numHashes: Int = 1
 
   val b: Int
 
   lazy val m: Int = 1 << (b - 1)
 
-  private lazy val alphamm: Double = b match {
+  protected lazy val alphamm: Double = b match {
     case 4 => 0.673
     case 5 => 0.697
     case 6 => 0.709
     case _ => (0.7213 / (1 + 1.079 / m)) * m * m
   }
 
-  protected def tag(d: Vector[Int]) = Tag[Vector[Int], T](d)
+  protected def tag(d: D) = Tag[D, T](d)
 
   protected def numLeadingZeros(hashedValue: Bits32): Int =
     Integer.numberOfLeadingZeros((hashedValue << b) | (1 << (b - 1)) + 1) + 1
@@ -39,23 +38,17 @@ with Monoid[Vector[Int] @@ T]{
 
   protected val negPow2to32 = -4294967296.0
 
-  def add(d: Vector[Int] @@ T, a: A)(implicit h: H, hconv: HC): Vector[Int] @@ T = {
-    val hashedValue = hashItem(a).head
+  protected def registerSum(d: D @@ T): Double
 
-    val j = readAddress(hashedValue)
+  protected def numZeroRegisters(d: D @@ T): Int
 
-    val r = numLeadingZeros(hashedValue)
-
-    tag((d(j) lt r) ? d.updated(j, r) | d)
-  }
-
-  def cardinality(d: Vector[Int] @@ T): Long = {
-    val rsum = d map ( count => math.pow(2, (-1 * count))) sum
+  def cardinality(d: D @@ T): Long = {
+    val rsum = registerSum(d)
 
     val estimate = alphamm / rsum
 
     val correctedEstimate = if(estimate lte (5.0 / 2.0 * m)){ // small range correction
-      val v = d.foldLeft(0)((acc, x) => (x === 0) ? (acc + 1) | acc)
+      val v = numZeroRegisters(d)
 
       (v =/= 0) ? (m * (math.log(m) - math.log(v))) | estimate
     }else if(estimate gt pow2to32) // large range correction
@@ -66,6 +59,25 @@ with Monoid[Vector[Int] @@ T]{
     math.round(correctedEstimate)
   }
 
+
+
+}
+
+
+
+trait DenseHyperLogLogT[A,H1,T]
+extends HyperLogLogT[A,H1,Vector[Int],T]{
+
+  def add(d: Vector[Int] @@ T, a: A)(implicit h: H, hconv: HC): Vector[Int] @@ T = {
+    val hashedValue = hashItem(a).head
+
+    val j = readAddress(hashedValue)
+
+    val r = numLeadingZeros(hashedValue)
+
+    tag((d(j) lt r) ? d.updated(j, r) | d)
+  }
+
   def equal(d1: Vector[Int] @@ T, d2: Vector[Int] @@ T) =
     d1 == d2
 
@@ -73,13 +85,20 @@ with Monoid[Vector[Int] @@ T]{
 
   def append (d1: Vector[Int] @@ T, d2: => Vector[Int] @@ T): Vector[Int] @@ T =
     tag(d1.zip(d2) map { case (r1, r2) => r1 max r2 })
+
+  protected def registerSum(d: Vector[Int] @@ T) =
+    d map ( count => math.pow(2, (-1 * count))) sum
+
+  protected def numZeroRegisters(d: Vector[Int] @@ T) =
+    d.foldLeft(0)((acc, x) => (x === 0) ? (acc + 1) | acc)
+
 }
 
 object hyperloglog{
   type HLLRegisters[T] = Vector[Int] @@ T
 
   object hll{
-    def apply[A,H1,T](addressSize: Int, s: Long = 0L) = new HyperLogLogT[A,H1,T]{
+    def dense[A,H1,T](addressSize: Int, s: Long = 0L) = new DenseHyperLogLogT[A,H1,T]{
       val seed = s
 
       val b = addressSize
