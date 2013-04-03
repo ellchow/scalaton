c30d369b9ea34885b8906764ea8bf63d18d9c62f
ellchow <drag0nxspirit@gmail.com>
2013-04-03 16:19:15 -0700

    refactor to remove need for reflexive calls

--- scalaton-doo/src/main/scala/scalaton/doo/Implicits.scala
+++ scalaton-doo/src/main/scala/scalaton/doo/Implicits.scala
@@ -30,47 +30,49 @@ trait EnrichedDList[A]{
 }
 
 trait ImplicitConversions{
-  implicit def enrichDListWithSample[A : Manifest : WireFormat](x: DList[A])(implicit hashable: Hashable[A,Bits32]) =
-    new EnrichedDList[A]{
-      val dl = x
 
-      def sample(rate: Double, seed: Int = 0) = sampling.sample(dl, rate, seed)
-    }
+  private[doo] case class DListWithSample[A : Manifest : WireFormat](val dl: DList[A])(implicit hashable: Hashable[A,Bits32]){
+    def sample(rate: Double, seed: Int = 0) = sampling.sample(dl, rate, seed)
+  }
+
+  private[doo] case class DListWithSampleBy[A : Manifest : WireFormat, B : Manifest : WireFormat](dl: DList[(A,B)])(implicit hashable: Hashable[A,Bits32]){
+    def sampleBy(rate: Double, seed: Int = 0) = sampling.sampleBy(dl, rate, seed)
+  }
+
+  private[doo] case class DListWithLimit[A : Manifest : WireFormat](val dl: DList[A]){
+    def limit(n: Int = 0) = sampling.limit(dl, n)
+  }
+
+  private[doo] case class DListWithPartitionAtRandom[A : Manifest : WireFormat](val dl: DList[A]){
+    def partitionAtRandom(n: Int, seed: Int = 0) = sampling.partitionAtRandom(dl, n, seed)
+  }
+
+  private[doo] case class DListWithBloomJoin[A : Manifest : WireFormat : Grouping, BL : Manifest : WireFormat](val dl: DList[(A, BL)])(implicit hashable: Hashable[A,Bits32]){
+    def bloomJoin[BR : Manifest : WireFormat](right: DList[(A,BR)], expectedNumKeys: Int) = joins.bloomJoin(dl, right, expectedNumKeys)
+  }
+
+  private[doo] case class DListWithSkewedJoin[A : Manifest : WireFormat : Grouping, BL : Manifest : WireFormat](val dl: DList[(A, BL)])(implicit hashable: Hashable[A,Bits32]){
+    def skewedJoin[BR : Manifest : WireFormat](right: DList[(A,BR)], sampleRate: Double, maxPerReducer: Int) = joins.skewedJoin(dl, right, sampleRate, maxPerReducer)
+  }
+
+
+  implicit def enrichDListWithSample[A : Manifest : WireFormat](x: DList[A])(implicit hashable: Hashable[A,Bits32]) =
+    DListWithSample(x)
 
   implicit def enrichDListWithSampleBy[A : Manifest : WireFormat, B : Manifest : WireFormat](x: DList[(A,B)])(implicit hashable: Hashable[A,Bits32]) =
-    new EnrichedDList[(A,B)]{
-      val dl = x
-
-      def sampleBy(rate: Double, seed: Int = 0) = sampling.sampleBy(dl, rate, seed)
-    }
+    DListWithSampleBy(x)
 
   implicit def enrichDListWithLimit[A : Manifest : WireFormat](x: DList[A]) =
-    new EnrichedDList[A]{
-      val dl = x
-
-      def limit(n: Int = 0) = sampling.limit(dl, n)
-    }
+    DListWithLimit(x)
 
   implicit def enrichDListWithPartitionAtRandom[A : Manifest : WireFormat](x: DList[A]) =
-    new EnrichedDList[A]{
-      val dl = x
-
-      def partitionAtRandom(n: Int, seed: Int = 0) = sampling.partitionAtRandom(dl, n, seed)
-    }
+    DListWithPartitionAtRandom(x)
 
   implicit def enrichDListWithBloomJoin[A : Manifest : WireFormat : Grouping, BL : Manifest : WireFormat](x: DList[(A, BL)])(implicit hashable: Hashable[A,Bits32]) =
-    new EnrichedDList[(A,BL)]{
-      val dl = x
-
-      def bloomJoin[BR : Manifest : WireFormat](right: DList[(A,BR)], expectedNumKeys: Int) = joins.bloomJoin(dl, right, expectedNumKeys)
-    }
+    DListWithBloomJoin(x)
 
   implicit def enrichDListWithSkewedJoin[A : Manifest : WireFormat : Grouping, BL : Manifest : WireFormat](x: DList[(A, BL)])(implicit hashable: Hashable[A,Bits32]) =
-    new EnrichedDList[(A,BL)]{
-      val dl = x
-
-      def skewedJoin[BR : Manifest : WireFormat](right: DList[(A,BR)], sampleRate: Double, maxPerReducer: Int) = joins.skewedJoin(dl, right, sampleRate, maxPerReducer)
-    }
+    DListWithSkewedJoin(x)
 
 }
 
