56789f5428caeb9c9da609eda42caa453220c761
ellchow <drag0nxspirit@gmail.com>
2013-04-04 19:10:28 -0700

    put data related helper in its own module

--- scalaton-doo/src/main/scala/scalaton/doo/Helpers.scala
+++ scalaton-doo/src/main/scala/scalaton/doo/Helpers.scala
@@ -16,14 +16,10 @@
 
 package scalaton.doo
 
-import scalaton.util.ProductFlatIterator
-
 import com.nicta.scoobi.Scoobi._
 
 import scalaz.{DList => _, _}
 import Scalaz._
-
-import org.slf4j.LoggerFactory
 
 trait HelperFunctions {
 
@@ -49,92 +45,6 @@ trait HelperFunctions {
   def parallelFoldMonoid[A : Manifest : WireFormat, B : Manifest : WireFormat : Monoid](dl: DList[A])(f: (B, A) => B) =
     parallelFold(dl, implicitly[Monoid[B]].zero)(f)
 
-  def cacheDList[A : Manifest : WireFormat : AvroSchema](dl: DList[A], path: String, overwrite: Boolean = false)(implicit sconf: ScoobiConfiguration): DList[A] = {
-    val logger = LoggerFactory.getLogger("cacheDList")
-
-    if(overwrite || !hdfs.exists(path) || !hdfs.isComplete(path)){
-      logger.info("computing and writing output to $path%s")
-      persist(toAvroFile(dl, path,overwrite=true))
-    }else{
-      logger.info("reading cached result from $path%s")
-      fromAvroFile(path)
-    }
-  }
-
-  def toDelimitedTextFileWithHeader[A <: Product : Manifest](dl: DList[A], header: Product,
-                                                             path: String, sep: String = "\t",
-                                                             noneString: String = "",
-                                                             encode: String => String = _.replaceAll("\\s+"," "),
-                                                             overwrite: Boolean = false,
-                                                             headerPath: String ="header",
-                                                             dataPath: String ="data")
-  = {
-    def unpackToString(prod: Product, hdr: Product): String = {
-      require(prod.productArity == hdr.productArity, "record and header must have same arity: $prod%s    $hdr%s")
-
-      prod.productIterator.zip(hdr.productIterator).map{
-        case (None, h: Product) =>
-          ProductFlatIterator(h).map(x => noneString).mkString(sep)
-
-        case (Some(elem: Product), h: Product) =>
-          unpackToString(elem, h)
-
-        case (elem: Product, h: Product) =>
-          unpackToString(elem, h)
-
-        case (None, h: String) => noneString
-
-        case (Some(elem), h: String) =>
-          encode(elem.toString)
-
-        case (elem: Any, h: String) => encode(elem.toString)
-      }.mkString(sep)
-    }
-
-    val out = dl.map{x => unpackToString(x, header)}
-
-    hdfs.write(path + "/" + headerPath, new ProductFlatIterator(header).mkString(sep) + "\n")
-
-    toTextFile(out, path + "/" + dataPath, overwrite)
-  }
-
-  def mapToDelimitedTextFileWithHeader(dl: DList[Map[String,String]], header: Seq[String],
-                                       path: String, sep: String = "\t",
-                                       defaultValue: Option[String] = None,
-                                       encode: String => String = _.replaceAll("\\s+"," "),
-                                       overwrite: Boolean = false,
-                                       headerPath: String ="header",
-                                       dataPath: String ="data")
-  = {
-
-    val out = dl.map{
-      tuple => header.map{
-        col => {
-          encode(defaultValue match {
-            case Some(x) => tuple.getOrElse(col, x)
-            case None => tuple(col)
-          })
-        }
-      }.mkString(sep)
-    }
-
-    hdfs.write(path + "/" + headerPath, header.mkString(sep) + "\n")
-
-    toTextFile(out, path + "/" + dataPath, overwrite)
-  }
-
-  def fromDelimitedTextFileWithHeader(path: String, sep: String = "\t", headerPath: String ="header", dataPath: String ="data")
-  : (Seq[String], DList[Map[String,String]]) = {
-    val header = hdfs.getLines(path + "/" + headerPath).head.split(sep).toSeq
-
-    val lines = fromTextFile(path + "/" + dataPath)
-
-    val dl = lines.map{ line => header.zip(line.split(sep)).toMap }
-
-    (header, dl)
-  }
-
 }
-
 
 object helpers extends HelperFunctions
