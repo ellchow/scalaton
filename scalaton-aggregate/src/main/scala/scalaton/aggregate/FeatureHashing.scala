31b4fa6b8fdf6f60afd5b4623972764d96428949
Elliot Chow <drag0nxspirit@gmail.com>
2013-05-18 17:38:15 -0700

    ngrams helper

--- scalaton-aggregate/src/main/scala/scalaton/aggregate/FeatureHashing.scala
+++ scalaton-aggregate/src/main/scala/scalaton/aggregate/FeatureHashing.scala
@@ -16,6 +16,9 @@
 
 package scalaton.aggregate
 
+import collection.immutable.{Vector => SVector}
+import scala.annotation.tailrec
+
 import scalaton.util._
 import scalaton.util.hashing._
 
@@ -41,6 +44,19 @@ trait FeatureHashingModule{
                           (implicit h: Hashable[A,H1], hconv: HashCodeConverter[H1,Bits32]): Seq[(Int,Double)] =
     features.map(f => hashFeature(f, min, max, seed)(h,hconv)).toSeq
 
+  def ngrams[A](features: Seq[A], n: Int = 2) = {
+    @tailrec
+    def loop(xs: SVector[Seq[A]], remaining: Seq[A]): SVector[Seq[A]]  =
+      if(remaining.nonEmpty && remaining.size >= n)
+        loop(xs :+ (remaining take n), remaining drop 1)
+      else
+        xs
+
+    loop(SVector[Seq[A]](), features)
+  }
+
+  // def combinations[A,B](aFeatures: List[A], bFeatures: List[B]) =
+    // (aFeatures |@| bFeatures) ((_, _))
 }
 
 object featurehash extends FeatureHashingModule
