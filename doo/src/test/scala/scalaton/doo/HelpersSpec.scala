d1d1461faa2965c9ec35375dbf926cc04efb1c7d
ellchow <drag0nxspirit@gmail.com>
2013-10-15 15:29:43 -0700

    simplify test

--- doo/src/test/scala/scalaton/doo/HelpersSpec.scala
+++ doo/src/test/scala/scalaton/doo/HelpersSpec.scala
@@ -19,15 +19,12 @@ import implicits._
 class HelpersSpec extends HadoopSpecification {
 
   "groupByKeyThenCombine should yield same results as groupByKey.combine" >>  { implicit conf: ScoobiConfiguration =>
-    for(_ <- 1 to 10)
-    yield{
-      val n = SRandom.nextInt(10000) + 1
-      val m = SRandom.nextInt(1000) + 1
-      val flushThreshold = SRandom.nextInt(n) + 1
+    val n = SRandom.nextInt(10000) + 1
+    val m = SRandom.nextInt(1000) + 1
+    val flushThreshold = SRandom.nextInt(n) + 1
 
-      val dl = DList((0 until n).map(_ => SRandom.nextInt()).zipWithIndex : _*)
+    val dl = DList((0 until n).map(_ => SRandom.nextInt()).zipWithIndex : _*)
 
-      dl.groupByKeyThenCombine(_.size gte flushThreshold).run.sorted must_== dl.groupByKey.combine((_: Int) |+| (_: Int)).run.sorted
-    }
+    dl.groupByKeyThenCombine(_.size gte flushThreshold).run.sorted must_== dl.groupByKey.combine((_: Int) |+| (_: Int)).run.sorted
   }
 }
