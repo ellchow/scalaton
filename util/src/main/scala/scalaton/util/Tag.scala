e0f31c037f9c42c3d79f8f540d61770a7a99347c
ellchow <zzzxqq@gmail.com>
2013-11-03 20:19:49 -0800

    missing Tag file

---
+++ util/src/main/scala/scalaton/util/Tag.scala
@@ -0,0 +1,26 @@
+/*
+ Copyright 2013 Elliot Chow
+
+ Licensed under the Apache License, Version 2.0 (the "License")
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package scalaton.util
+
+import scalaz._
+import Scalaz._
+
+trait Tagging{
+ implicit class Tagger[A](a: A){ def tag[T]: A @@ T = Tag(a) }
+}
+
+object tag extends Tagging
