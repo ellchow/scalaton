938042bce79782f4569199ace7a23491a86ae73a
ellchow <zzzxqq@gmail.com>
2013-12-23 23:37:49 -0800

    use validationnel

--- util/src/main/scala/scalaton/util/parsing/CsvParser.scala
+++ util/src/main/scala/scalaton/util/parsing/CsvParser.scala
@@ -1,10 +1,10 @@
 package scalaton.util.parsing
 
 import scala.util.parsing.combinator._
+import scalaz._, Scalaz._
 
 trait CsvParser extends RegexParsers {
   override val skipWhitespace = false
-  // override val whiteSpace = """\s+""".r
 
   val DELIMITER: String = ","
   val QUOTE: String = "\""
@@ -12,7 +12,8 @@ trait CsvParser extends RegexParsers {
   val LF: String = "\n"
 
   def CRLF = CR + LF
-  def rowDelimiter = CR | LF | CRLF
+
+  def rowDelimiter: Parser[String] = CR | LF | CRLF
 
   def wholeNumber: Parser[String] = """-?\d+""".r
 
@@ -23,19 +24,21 @@ trait CsvParser extends RegexParsers {
 
   def quoted: Parser[String] = QUOTE ~> s"[^${QUOTE}]*".r <~ QUOTE
 
-  def row: Parser[List[String]] = repsep(quoted | unquoted, DELIMITER)
+  def field: Parser[String] = quoted | unquoted
+
+  def row: Parser[List[String]] = repsep(field, DELIMITER)
 
   def header = row
 
   def rows: Parser[List[List[String]]] = repsep(row, rowDelimiter)
 
-  def fromString: Parser[List[Option[Map[String,String]]]] = for{
+  def fromString: Parser[List[ValidationNel[String,Map[String,String]]]] = for{
     hdr <- header <~ rowDelimiter
     size = hdr.size
     rs <- rows
-  } yield rs.map{ r =>
-    if(r.size != size) None
-    else Some(hdr zip r toMap)
+  } yield rs.zipWithIndex.map{ case (r, i) =>
+      if(r.size != size) s"invalid number of fields at line $i (got ${r.size}, expected $size)".failureNel
+      else (hdr zip r toMap).successNel
   }
 
   def parse(s: String) = parseAll(fromString, s)
