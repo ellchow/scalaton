c81286dca51acfeaf82afbd3cef359d53fa3b906
ellchow <zzzxqq@gmail.com>
2014-03-25 21:28:58 -0700

    basic tracing stats

--- async/src/main/scala/scalaton/async/akka/tracing/TracingModule.scala
+++ async/src/main/scala/scalaton/async/akka/tracing/TracingModule.scala
@@ -19,20 +19,23 @@ package scalaton.async.akka.tracing
 import akka.actor._
 import com.github.nscala_time.time.Imports._
 import scalaton.async.akka._
+import scalaz._, Scalaz._
 
 trait TracingModule extends Akka {
   import TraceAggregator._
 
   implicit class TracingOps(receiver: ActorRef) {
     def !+[A](msg: A)(implicit sender: ActorRef = Actor.noSender): Unit = {
-      traceAggregator ! SendTrace(Option(sender), receiver, System.currentTimeMillis, msg)
+      if (turnOnTracing)
+        traceAggregator ! SendTrace(Option(sender), receiver, System.currentTimeMillis, msg)
       receiver ! msg
     }
 
     def tell_+[A](msg: A, sender: ActorRef) = !+(msg)(sender)
 
     def >+[A](msg: A)(implicit context: ActorContext): Unit = {
-      traceAggregator ! ForwardTrace(Option(context.sender), receiver, context.self, System.currentTimeMillis, msg)
+      if (turnOnTracing)
+        traceAggregator ! ForwardTrace(Option(context.sender), receiver, context.self, System.currentTimeMillis, msg)
       receiver forward msg
     }
 
@@ -53,64 +56,81 @@ trait TracingModule extends Akka {
     }
   }
 
+  def turnOnTracing = true
   lazy val traceAggregator: ActorRef = system.actorOf(Props[TraceAggregator], "trace-aggregator")
 }
 
 object TraceAggregator {
-  case class SendTrace[A](val sender: Option[ActorRef], val receiver: ActorRef, val timestamp: Long , val msg: A) {
+  sealed trait MessageTrace[A] {
+    val sender: Option[ActorRef]
+    val receiver: ActorRef
+    val timestamp: Long
+    val msg: A
+    val msgType: String
+    val traceType: String
+    def prettify: String
+  }
+
+  case class SendTrace[A](val sender: Option[ActorRef], val receiver: ActorRef, val timestamp: Long , val msg: A, val msgTypeOption: Option[String] = None) extends MessageTrace[A] {
+    lazy val msgType = msgTypeOption.getOrElse(msg.getClass.getName)
+    val traceType = "send"
+
     def prettify = {
       // val t = (new DateTime(System.currentTimeMillis)).toString(org.joda.time.format.ISODateTimeFormat.dateTime)
       val s = sender.map(_.path.name).getOrElse("<NA>")
-      s"SEND    | $timestamp | sender:$s | receiver:${receiver.path.name} | $msg"
+      s"| TRACE:SEND | $timestamp | sender:$s | receiver:${receiver.path.name} | $msg"
     }
   }
 
-  case class ReceiveTrace(val sender: Option[ActorRef], val receiver: ActorRef, val timestamp: Long, val msg: Any) {
+  case class ReceiveTrace(val sender: Option[ActorRef], val receiver: ActorRef, val timestamp: Long, val msg: Any, val msgTypeOption: Option[String] = None) extends MessageTrace[Any] {
+    lazy val msgType = msgTypeOption.getOrElse(msg.getClass.getName)
+    val traceType = "receive"
     def prettify = {
       val s = sender.map(_.path.name).getOrElse("<NA>")
-      s"RECEIVE | $timestamp | sender:${s} | receiver:${receiver.path.name} | $msg"
+      s"| TRACE:RECEIVE | $timestamp | sender:${s} | receiver:${receiver.path.name} | $msg"
     }
   }
 
-  case class ForwardTrace[A](val sender: Option[ActorRef], val receiver: ActorRef, val through: ActorRef, val timestamp: Long , val msg: A) {
+  case class ForwardTrace[A](val sender: Option[ActorRef], val receiver: ActorRef, val through: ActorRef, val timestamp: Long , val msg: A, val msgTypeOption: Option[String] = None) extends MessageTrace[A] {
+    lazy val msgType = msgTypeOption.getOrElse(msg.getClass.getName)
+    val traceType = "forward"
+
     def prettify = {
       val s = sender.map(_.path.name).getOrElse("<NA>")
-      s"FORWARD | $timestamp | sender:${s} | through:${through.path.name} | receiver:${receiver.path.name} | $msg"
+      s"| TRACE:FORWARD | $timestamp | sender:${s} | through:${through.path.name} | receiver:${receiver.path.name} | $msg"
     }
   }
 }
 class TraceAggregator extends Actor with ActorLogging {
   import TraceAggregator._
 
+  val stats = context.actorOf(Props(new TraceStats), "trace-stats")
+
   val receive: Receive = {
-    case st: SendTrace[_] =>
-      log.debug(st.prettify)
-
-    case rt: ReceiveTrace =>
-      log.debug(rt.prettify)
-
-    case ft: ForwardTrace[_] =>
-      log.debug(ft.prettify)
+    case messageTrace: MessageTrace[_] =>
+      log.info(messageTrace.prettify)
+      stats ! messageTrace
   }
 }
 
-/*
-import scalaton.async.akka.tracing._
-import akka.actor._
-import scala.concurrent._, duration._
-
-lazy val sys = ActorSystem("system")
-
-object Main extends TracingModule {
- lazy val system = sys
-
- val a = system.actorOf(Props(new TracingActor { val receive: Receive = tracedReceive({ case msg => println(("forward", msg)); c >+ msg })}))
- val b = system.actorOf(Props(new Actor { val receive: Receive = { case _ =>  } }))
- val c = system.actorOf(Props(new TracingActor { val receive: Receive = tracedReceive({ case msg => println(("echo", msg)) })}))
+object TraceStats {
+  case object GetOverallMessageCounts
+  case class OverallMessageCounts(val counts: Map[String,Map[String,Long]])
 }
-import Main._
+class TraceStats extends Actor with ActorLogging {
+  import TraceAggregator._
+  import TraceStats._
 
-c !+ "hello"
-a !+ "hello"
+  var overallMessageCounts = Map.empty[String,Map[String,Long]] // message type -> trace type -> Long
 
-*/
+  lazy val receive: Receive = processMessage orElse handleRequest
+
+  val processMessage: Receive = {
+    case messageTrace: MessageTrace[_] =>
+      overallMessageCounts = overallMessageCounts |+| Map(messageTrace.msgType -> Map(messageTrace.traceType -> 1))
+  }
+
+  val handleRequest: Receive = {
+    case GetOverallMessageCounts => sender ! OverallMessageCounts(overallMessageCounts)
+  }
+}
